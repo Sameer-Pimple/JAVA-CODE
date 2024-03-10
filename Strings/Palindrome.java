41ebcbabbe878831f1af3a6830552694d718cf27
Sameer-Pimple <sameerpimple2002@gmail.com>
2024-03-10 05:33:46 +0530

    new

---
+++ Strings/Palindrome.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Palindrome {
+
+    public static boolean PALINDROME(String str){
+        for(int i=0; i<str.length()/2; i++){
+            if (str.charAt(i)!=str.charAt(str.length()-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        String str="nayan";
+        System.out.println(PALINDROME(str));
+    }
+    
+}
